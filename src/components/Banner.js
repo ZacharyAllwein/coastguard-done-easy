bcc102db09ada1d37f1dc837b737132860f49e2d
ZacharyAllwein <zrallwein@gmail.com>
2022-02-14 16:18:57 -0600

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,13 @@
+import anchorWithWings from "../assets/anchorWithWings.png"
+
+function Banner(){
+
+    return (
+        <div className="banner">
+            <h1>Coastguard Done Easy</h1>
+            <img src={anchorWithWings} />
+        </div>
+    )
+}
+
+export default Banner;
