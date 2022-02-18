d26b272120ced3913831806434340bfceeee8d02
ZacharyAllwein <zrallwein@gmail.com>
2022-02-18 16:19:43 -0600

    sad

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,13 @@
-import anchorWithWings from "../assets/anchorWithWings.png"
+import anchorWithWings from "../assets/anchorWithWings.png";
 
-function Banner(){
-
-    return (
-            <div className= "banner">
-                <h1>Coastguard Done Easy</h1>
-                <img src={anchorWithWings} />
-                <h2>Riding Tides to Save Lives</h2>
-            </div>
-    )
+function Banner() {
+  return (
+    <div className="banner">
+      <h1>Coastguard Done Easy</h1>
+      <img src={anchorWithWings} />
+      <h2>Riding Tides to Save Lives</h2>
+    </div>
+  );
 }
 
 export default Banner;
