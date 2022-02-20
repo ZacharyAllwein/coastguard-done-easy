60965a250d695b858c2d292102aeaa65e348d0b5
Riot Xana <zrallwein@gmail.com>
2022-02-20 15:25:27 -0600

    finalized blur effect

--- src/App.js
+++ src/App.js
@@ -1,50 +1,62 @@
 import Banner from "./components/Banner.js";
 import Reason from "./components/Reason.js";
+import { useRef } from "react";
 
-let activeReason = null;
+let reasonActive = false;
 function App() {
+  let placeholder = useRef(null);
+  let main = useRef(null);
 
-  function handleClick(e){
+  function handleClick(e) {
+    let element = e.target;
+    let pH = placeholder.current;
 
-    let element = e.target
-    if(element.parentNode.classList.contains("reason") || element.parentNode.classList.contains("reason-big")) element = element.parentNode;
+    if (
+      element.parentNode.classList.contains("reason") ||
+      element.parentNode.classList.contains("reason-big")
+    )
+      element = element.parentNode;
 
-    if(activeReason === null){
+    if (!reasonActive) {
+      if (element.classList.contains("reason")) {
+        pH.innerHTML = element.innerHTML;
 
-      if(element.classList.contains("reason")){
-        element.classList.remove("reason")
-        element.classList.add("reason-big")
-  
-        activeReason = element
+        pH.classList.add("reason-big");
+        main.current.classList.add("blur");
+
+        reasonActive = true;
       }
+    } else if (reasonActive && !element.classList.contains("reason-big")) {
+      pH.innerHTML = "<div />";
+
+      pH.classList.remove("reason-big");
+      main.current.classList.remove("blur");
+
+      reasonActive = false;
     }
-    else if(activeReason !== null && !element.classList.contains("reason-big")){
-      activeReason.classList.remove("reason-big")
-      activeReason.classList.add("reason")
-      activeReason = null;
-    }
-    
-    
   }
   return (
-    <div onClick={(e) => handleClick(e)}>
-      <Banner />
-      <div className="reasons-grid">
-        <Reason
-          image="anchor.jpg"
-          text="Lorum Ipsum Dolor I don't know waht to put here this is an experiment, wulllllll. HOw are you doing today"
-        />
-        <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
-        <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
-        <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
-        <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
-        <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
-        <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
-        <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
-        <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
-        <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
-        <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
-        <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
+    <div>
+      <div ref={placeholder} />
+      <div ref={main} onClick={(e) => handleClick(e)}>
+        <Banner />
+        <div className="reasons-grid">
+          <Reason
+            image="anchor.jpg"
+            text="Lorum Ipsum Dolor I don't know waht to put here this is an experiment, wulllllll. HOw are you doing today"
+          />
+          <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
+          <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
+          <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
+          <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
+          <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
+          <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
+          <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
+          <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
+          <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
+          <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
+          <Reason image="anchor.jpg" text="Lorum Ipsum Dolor" />
+        </div>
       </div>
     </div>
   );
