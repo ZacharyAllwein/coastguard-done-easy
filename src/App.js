06f29f99be047bda4c6b824c6763141ab144ae06
Riot Xana <zrallwein@gmail.com>
2022-02-23 19:22:04 -0600

    cleaned up code and added template data

--- src/App.js
+++ src/App.js
@@ -1,53 +1,56 @@
 import Banner from "./components/Banner.js";
 import Reason from "./components/Reason.js";
-import ReasonBig from "./components/ReasonBig.js"
+import ReasonBig from "./components/ReasonBig.js";
 import jobs from "./data/jobs.js";
 import { useRef, useState } from "react";
 
 function App() {
   let main = useRef(null);
 
-  const [reasonBigStates, setReasonBigStates] = useState(null)
+  const [reasonBigStates, setReasonBigStates] = useState(null);
 
   function handleClick(e) {
     let element = e.target;
 
     if (!element.classList.contains("reason")) {
-      while (element.parentNode) {
-        element = element.parentNode;
-        if (
+      while (
+        !(
           element.classList.contains("reason") ||
-          element.classList.contains("reason-big")
-        ) {
-          break;
-        } else if (element.classList.contains("root")) {
-          break;
-        }
-      }
+          element.classList.contains("reason-big") ||
+          element.classList.contains("root")
+        )
+      )
+        element = element.parentNode;
     }
 
     if (!reasonBigStates) {
       if (element.classList.contains("reason")) {
-
         const job = element.id;
+        console.log(element.id);
 
-        setReasonBigStates({title: job, primaryImage: jobs[job].primaryImage, text: jobs[job].text})
+        setReasonBigStates({
+          title: job,
+          primaryImage: jobs[job].primaryImage,
+          text: jobs[job].text,
+        });
 
         main.current.classList.add("blur");
-
-        // reasonActive = true;
       }
     } else if (reasonBigStates && !element.classList.contains("reason-big")) {
-
       main.current.classList.remove("blur");
-      setReasonBigStates(null)
-
+      setReasonBigStates(null);
     }
   }
-  
+
   return (
     <div className="root">
-      {reasonBigStates && <ReasonBig title={reasonBigStates.title} primaryImage={reasonBigStates.primaryImage} text={reasonBigStates.text} />}
+      {reasonBigStates && (
+        <ReasonBig
+          title={reasonBigStates.title}
+          primaryImage={reasonBigStates.primaryImage}
+          text={reasonBigStates.text}
+        />
+      )}
       <div ref={main} onClick={(e) => handleClick(e)}>
         <Banner />
         <p className="blog">
@@ -62,7 +65,7 @@ function App() {
               <Reason
                 title={title}
                 image={jobs[title].primaryImage}
-                text={jobs["Aviation Maintenance Technician"].text}
+                text={jobs[title].blurb}
                 key={index}
               />
             );
--- src/data/jobs.js
+++ src/data/jobs.js
@@ -1,61 +1,117 @@
 const jobs = {
   "Aviation Maintenance Technician": {
     primaryImage: "assets/aviMain.jpg",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
     text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
   "Aviation Survival Technician": {
     primaryImage: "assets/aviSurv.jpg",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
+    text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
   "Avionics Electrical Technician": {
     primaryImage: "assets/aviElec.jpg",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
+    text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
   "Boatswain's Mate": {
     primaryImage: "assets/boaMate.jpg",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
+    text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
   "Culinary Specialist": {
     primaryImage: "assets/culSpec.jpg",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
+    text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
   "Damage Controlman": {
     primaryImage: "assets/damCont.jpg",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
+    text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
   "Electrician's Mate": {
     primaryImage: "assets/eleMate.jpg",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
+    text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
   "Electronics Technician": {
     primaryImage: "assets/eleTech.png",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
+    text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
   "Gunner's Mate": {
     primaryImage: "assets/gunMate.jpg",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
+    text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
   "Health Services Technician": {
     primaryImage: "assets/heaServ.jpg",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
+    text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
   "Information Systems Technician": {
     primaryImage: "assets/infTech.jpg",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
+    text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
   "Intelligence Specialist": {
     primaryImage: "assets/inteSpec.jpg",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
+    text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
   "Machinery Technician": {
     primaryImage: "assets/macTech.jpg",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
+    text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
   "Marine Science Technician": {
     primaryImage: "assets/marTech.jpg",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
+    text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
   "Maritime Enforcement Specialist": {
     primaryImage: "assets/marEnfo.png",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
+    text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
   "Operations Specialist": {
     primaryImage: "assets/opeSpec.jpg",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
+    text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
   "Public Affairs Specialist": {
     primaryImage: "assets/pubAffa.jpg",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
+    text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
   Storekeeper: {
     primaryImage: "assets/stoKeep.jpg",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
+    text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
   Yeoman: {
     primaryImage: "assets/humReso.jpg",
+    blurb:
+      "Aviation Maintenance Tehcnician's handle all things aircraft mechanics and crewing in the coastguard.",
+    text: "If you have always been interested in aircraft or mechanics, you might enjoy being an Aviation Maintenance Technician or AMT for short. AMT's are responsible for the maintenance and repair of all aircraft systems including: aircraft engines, auxiliary power units, propellers, rotor systems, power train systems, and associated airframe and systems-specific electrical components. Along with acting as an aircraft mechanic, AMT's also serve on different positions within a coastguard flight crew acting as an AMT or any position that flight crew might need at the time.",
   },
 };
 
